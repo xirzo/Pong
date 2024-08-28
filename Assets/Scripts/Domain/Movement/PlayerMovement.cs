8cfa505f183d648a687ba0620e0884247e101ee9
Alex <EastTem@gmail.com>
2024-08-28 11:51:10 +0300

    refact: injecting camera bounds calculator

--- Assets/Scripts/Domain/Movement/PlayerMovement.cs
+++ Assets/Scripts/Domain/Movement/PlayerMovement.cs
@@ -1,47 +1,55 @@
-using System;
 using Pong.Domain.Bounds;
 using Pong.Domain.Inputs;
 using UnityEngine;
+using Zenject;
 
 namespace Pong.Domain.Movement
 {
-    [RequireComponent(typeof(PlayerInput))]
-    public class PlayerMovement : MonoBehaviour
-    {
-        private const float SCALE_MULTIPLIER = 0.5f;
-        [SerializeField] private CameraBoundsCalculator _cameraBoundsCalculator;
-        [SerializeField, Range(0, 1)] private float _speedMultiplier = 0.5f;
-        private PlayerInput _input;
-        private float _movementInputY;
-        private float _boundsY;
+	[RequireComponent(typeof(PlayerInput))]
+	public class PlayerMovement : MonoBehaviour
+	{
+		private const float SCALE_MULTIPLIER = 0.5f;
+		[SerializeField] [Range(0, 1)] private float _speedMultiplier = 0.5f;
+		private float _boundsY;
+		private CameraBoundsCalculator _cameraBoundsCalculator;
+		private PlayerInput _input;
+		private float _movementInputY;
 
-        private void Awake()
-        {
-            TryGetComponent(out _input);
-        }
+		private void Awake()
+		{
+			TryGetComponent(out _input);
+		}
 
-        private void Start()
-        {
-            _boundsY = _cameraBoundsCalculator.GetTopY();
-        }
+		private void Start()
+		{
+			_boundsY = _cameraBoundsCalculator.GetTopY();
+		}
 
-        private void UpdateInput()
-        {
-            _movementInputY = _input.Actions.Player.Movement.ReadValue<float>();
-        }
+		private void Update()
+		{
+			UpdateInput();
+			Move();
+		}
 
-        private void Move()
-        {
-            var positionOffset = transform.localScale.y * new Vector3(0, _movementInputY / 2) * _speedMultiplier;
+		[Inject]
+		private void Construct(CameraBoundsCalculator cameraBoundsCalculator)
+		{
+			_cameraBoundsCalculator = cameraBoundsCalculator;
+		}
 
-            transform.position += positionOffset;
-            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -(_boundsY - transform.localScale.y * SCALE_MULTIPLIER), _boundsY - transform.localScale.y * SCALE_MULTIPLIER), transform.position.z);
-        }
+		private void UpdateInput()
+		{
+			_movementInputY = _input.Actions.Player.Movement.ReadValue<float>();
+		}
 
-        private void Update()
-        {
-            UpdateInput();
-            Move();
-        }
-    }
+		private void Move()
+		{
+			var positionOffset = transform.localScale.y * new Vector3(0, _movementInputY / 2) * _speedMultiplier;
+
+			transform.position += positionOffset;
+			transform.position = new Vector3(transform.position.x,
+				Mathf.Clamp(transform.position.y, -(_boundsY - transform.localScale.y * SCALE_MULTIPLIER),
+					_boundsY - transform.localScale.y * SCALE_MULTIPLIER), transform.position.z);
+		}
+	}
 }
---
+++ Assets/Scripts/Installers/BoundsInstaller.cs
@@ -0,0 +1,16 @@
+using Pong.Domain.Bounds;
+using UnityEngine;
+using Zenject;
+
+namespace Pong.Installers
+{
+	public class BoundsInstaller : MonoInstaller
+	{
+		[SerializeField] private CameraBoundsCalculator _cameraBoundsCalculator;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<CameraBoundsCalculator>().FromInstance(_cameraBoundsCalculator).AsSingle();
+		}
+	}
+}
