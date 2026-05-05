e9e40ead478a1c9f5f8820d3df19d933ea60c348
xirzo <easttem@gmail.com>
2026-05-05 22:09:41 +0300

    feat: enhance repel

--- Assets/Scripts/Domain/Entities/Ball.cs
+++ Assets/Scripts/Domain/Entities/Ball.cs
@@ -5,39 +5,105 @@ using UnityEngine;
 
 namespace Pong.Domain.Entities
 {
-	public class Ball : MonoBehaviour, ILoser
-	{
-		[SerializeField] private LayerMask _repulseLayer;
-		private BallMovement _ballMovement;
-		private Vector3 _startPosition;
+    public class Ball : MonoBehaviour, ILoser
+    {
+        private const float HitCooldown = 0.05f;
 
-		private void Awake()
-		{
-			_startPosition = transform.position;
-		}
+        public event Action OnCollide;
 
+        [SerializeField] private LayerMask repulseLayer;
+        [SerializeField] private LayerMask wallLayer;
 
-		private void OnCollisionEnter2D(Collision2D other)
-		{
-			OnCollide?.Invoke();
+        private BallMovement _ballMovement;
+        private Vector3 _startPosition;
+        private EntityId? _lastHitPaddleId;
+        private float _lastHitTime;
 
-			if (((1 << other.gameObject.layer) & _repulseLayer) == 0) return;
+        private void Awake()
+        {
+            _startPosition = transform.position;
+        }
 
-			_ballMovement.CalculateReflectionAndSetDirection(other.contacts[0].normal);
-		}
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            bool isWall = ((1 << other.gameObject.layer) & wallLayer) != 0;
 
-		public void Reset()
-		{
-			transform.position = _startPosition;
-			_ballMovement.ResetVelocity();
-			_ballMovement.SetRandomDirection();
-		}
+            if (isWall)
+            {
+                _ballMovement.OnWallHit();
+                _ballMovement.CalculateReflectionAndSetDirection(other.contacts[0].normal);
+                OnCollide?.Invoke();
+                return;
+            }
+            
+            if (((1 << other.gameObject.layer) & repulseLayer) == 0)
+            {
+                return;
+            }
 
-		public void Construct(BallMovement ballMovement)
-		{
-			_ballMovement = ballMovement;
-		}
+            if (!IsValidPaddleHit(other))
+            {
+                return;
+            }
 
-		public event Action OnCollide;
-	}
+            if (IsDuplicateHit(other))
+            {
+                return;
+            }
+
+            OnCollide?.Invoke();
+
+            _ballMovement.CalculateReflectionAndSetDirection(other.contacts[0].normal);
+        }
+
+        private bool IsValidPaddleHit(Collision2D other)
+        {
+            Vector2 ballDirection = _ballMovement.GetCurrentDirection();
+
+            float paddlePositionX = other.transform.position.x;
+            float paddleFacingDirection = paddlePositionX < 0 ? 1f : -1f;
+
+            bool isMovingTowardFrontFace = Mathf.Approximately(Mathf.Sign(ballDirection.x), -paddleFacingDirection);
+
+            if (!isMovingTowardFrontFace)
+            {
+                Vector3 pos = transform.position;
+                pos.x += paddleFacingDirection * 0.1f;
+                transform.position = pos;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsDuplicateHit(Collision2D other)
+        {
+            EntityId paddleId = other.gameObject.GetEntityId();
+            float currentTime = Time.time;
+
+            if (_lastHitPaddleId.HasValue && paddleId == _lastHitPaddleId.Value &&
+                currentTime - _lastHitTime < HitCooldown)
+            {
+                return true;
+            }
+
+            _lastHitPaddleId = paddleId;
+            _lastHitTime = currentTime;
+            return false;
+        }
+
+        public void Reset()
+        {
+            transform.position = _startPosition;
+            _ballMovement.ResetVelocity();
+            _ballMovement.SetRandomDirection();
+            _lastHitPaddleId = null;
+            _lastHitTime = 0f;
+        }
+
+        public void Construct(BallMovement ballMovement)
+        {
+            _ballMovement = ballMovement;
+        }
+    }
 }
--- Assets/Scripts/Domain/Movement/PlayerMovement.cs
+++ Assets/Scripts/Domain/Movement/PlayerMovement.cs
@@ -5,48 +5,48 @@ using Zenject;
 
 namespace Pong.Domain.Movement
 {
-	public class PlayerMovement : IFixedTickable
-	{
-		private const float SCALE_MULTIPLIER = 0.5f;
-		private readonly float _boundsY;
-		private readonly float _speedMultiplier = 0.4f;
-		private readonly Transform _transform;
-		private readonly CameraBoundsCalculator _cameraBoundsCalculator;
-		private readonly PlayerInput _input;
-		private readonly PlayerSide _playerSide;
-		private float _movementInputY;
+    public class PlayerMovement : IFixedTickable
+    {
+        private const float ScaleMultiplier = 0.5f;
 
-		[Inject]
-		private PlayerMovement(CameraBoundsCalculator cameraBoundsCalculator, PlayerInput input, Transform transform, PlayerSide playerSide)
-		{
-			_cameraBoundsCalculator = cameraBoundsCalculator;
-			_input = input;
-			_transform = transform;
-			_playerSide = playerSide;
-			_boundsY = _cameraBoundsCalculator.GetTopY();
-		}
+        private readonly float _boundsY;
+        private readonly float _speedMultiplier = 0.4f;
+        private readonly Transform _transform;
+        private readonly PlayerInput _input;
+        private readonly PlayerSide _playerSide;
+        private float _movementInputY;
 
-		public void FixedTick()
-		{
-			UpdateInput();
-			Move();
-		}
+        [Inject]
+        private PlayerMovement(CameraBoundsCalculator cameraBoundsCalculator, PlayerInput input, Transform transform,
+            PlayerSide playerSide)
+        {
+            _input = input;
+            _transform = transform;
+            _playerSide = playerSide;
+            _boundsY = cameraBoundsCalculator.GetTopY();
+        }
 
-		private void UpdateInput()
-		{
-			_movementInputY = _playerSide == PlayerSide.Left
-				? _input.Actions.Players.LeftMovement.ReadValue<float>()
-				: _input.Actions.Players.RightMovement.ReadValue<float>();
-		}
+        public void FixedTick()
+        {
+            UpdateInput();
+            Move();
+        }
 
-		private void Move()
-		{
-			var positionOffset = new Vector3(0, _movementInputY / 2) * (_transform.localScale.y * _speedMultiplier);
+        private void UpdateInput()
+        {
+            _movementInputY = _playerSide == PlayerSide.Left
+                ? _input.Actions.Players.LeftMovement.ReadValue<float>()
+                : _input.Actions.Players.RightMovement.ReadValue<float>();
+        }
 
-			_transform.position += positionOffset;
-			_transform.position = new Vector3(_transform.position.x,
-				Mathf.Clamp(_transform.position.y, -(_boundsY - _transform.localScale.y * SCALE_MULTIPLIER),
-					_boundsY - _transform.localScale.y * SCALE_MULTIPLIER), _transform.position.z);
-		}
-	}
+        private void Move()
+        {
+            var positionOffset = new Vector3(0, _movementInputY / 2) * (_transform.localScale.y * _speedMultiplier);
+
+            _transform.position += positionOffset;
+            _transform.position = new Vector3(_transform.position.x,
+                Mathf.Clamp(_transform.position.y, -(_boundsY - _transform.localScale.y * ScaleMultiplier),
+                    _boundsY - _transform.localScale.y * ScaleMultiplier), _transform.position.z);
+        }
+    }
 }
